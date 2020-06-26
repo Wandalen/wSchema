386ee1d393d38f501351c9008057ef07e43a7fb9
wandalen <wandalen.me@gmail.com>
2020-06-26 16:27:23 +0300

    .

---
+++ proto/dwtools/amid/l1/schema/Include.s
@@ -0,0 +1,15 @@
+( function _Include_s_( ) {
+
+'use strict';
+
+if( typeof module !== 'undefined' )
+{
+
+  let _ = require( './include/Mid.s' );
+
+}
+
+if( typeof module !== 'undefined' )
+module[ 'exports' ] = _global_.wTools;
+
+})();
